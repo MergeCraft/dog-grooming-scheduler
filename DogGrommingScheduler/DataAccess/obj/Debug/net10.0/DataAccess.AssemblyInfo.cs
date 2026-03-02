33ca0bec6959814672d09834ce700720612e20e0
ManuelMM29 <158501393+ManuelMM29@users.noreply.github.com>
2026-03-02 11:51:05 -0300

    Add gitignore to develop

--- DogGrommingScheduler/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
+++ DogGrommingScheduler/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df0ebbb30504328f82ef5ec74dd1ddda32a6163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22594aab5967320ea35abb456df9b51365c4d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DogGrommingScheduler/DogGrommingScheduler/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ DogGrommingScheduler/DogGrommingScheduler/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df0ebbb30504328f82ef5ec74dd1ddda32a6163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22594aab5967320ea35abb456df9b51365c4d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
